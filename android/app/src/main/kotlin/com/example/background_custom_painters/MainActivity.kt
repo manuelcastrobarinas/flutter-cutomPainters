b8c1a69f30cee74d042e86339015561a7ea1d14d
manuelcastrobarinas <manuel.castrobarinas@gmail.com>
2024-03-07 09:53:37 -0500

    add painters

---
+++ android/app/src/main/kotlin/com/example/background_custom_painters/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.background_custom_painters
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/pages/headers_page.dart
@@ -0,0 +1,13 @@
+import 'package:background_custom_painters/src/widgets/headers.dart';
+import 'package:flutter/material.dart';
+
+class HeadersPage extends StatelessWidget {
+  const HeadersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: HeaderWaves()
+    );
+  }
+}
